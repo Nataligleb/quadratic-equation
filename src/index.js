c790ccbda160f038dc8f0035e42aefd8ed5af302
name <nata.mozhaueva@gmail.com>
2018-09-30 00:54:59 +0300

    nata

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+let arr=equation.match(/[-+]?[" "]?\d{1,}/g);
+let a = +arr[0];
+let b = +(arr[2][0] + arr[2].substring(2));
+let c = +(arr[3][0] + arr[3].substring(2));
+let d = b*b - 4*a*c;
+let res = [(Math.round(-b - Math.sqrt(d)) / (2 * a)), Math.round((-b + Math.sqrt(d)) / (2 * a))];
+return res.sort((a,b) => {return a - b});
 }
+
